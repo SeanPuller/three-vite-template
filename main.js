e9f6c84094900c2ed9d57f89f81f440b51b878cd
Sean Puller <43151199+SeanPuller@users.noreply.github.com>
2024-07-15 21:34:55 +0200

    add glb and fix loading

--- main.js
+++ main.js
@@ -1,24 +1,74 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm//loaders/GLTFLoader.js';
 
-setupCounter(document.querySelector('#counter'))
+let camera, scene, renderer;
+
+init();
+
+function init() {
+
+	const container = document.createElement( 'div' );
+	document.body.appendChild( container );
+
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
+	camera.position.set( - 1.8, 0.6, 2.7 );
+
+	scene = new THREE.Scene();
+
+	const loader = new GLTFLoader().setPath( './src/gltf/' );
+
+	loader.load( 'Megaphone.glb', async function ( gltf ) {
+
+		const model = gltf.scene;
+
+		// wait until the model can be added to the scene without blocking due to shader compilation
+
+		await renderer.compileAsync( model, camera, scene );
+
+		scene.add( model );
+
+		render();
+
+	} );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.toneMapping = THREE.ACESFilmicToneMapping;
+	renderer.toneMappingExposure = 1;
+	container.appendChild( renderer.domElement );
+
+	const controls = new OrbitControls( camera, renderer.domElement );
+	controls.addEventListener( 'change', render ); // use if there is no animation loop
+	controls.minDistance = 2;
+	controls.maxDistance = 10;
+	controls.target.set( 0, 0, - 0.2 );
+	controls.update();
+
+	const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+	scene.add( light );
+
+	window.addEventListener( 'resize', onWindowResize );
+
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	render();
+
+}
+
+//
+
+function render() {
+
+	renderer.render( scene, camera );
+
+}
